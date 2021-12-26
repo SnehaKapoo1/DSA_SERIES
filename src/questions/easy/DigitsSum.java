cc2e875d0fbc532d391f973f4eb2d0f6da5d2c6d
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2021-12-26 14:33:04 +0530

    ..!!

--- src/questions/easy/DigitsSum.java
+++ src/questions/easy/DigitsSum.java
@@ -2,19 +2,24 @@ package questions.easy;
 
 public class DigitsSum {
 
-    int digit, sum=0;
+//    int digit, sum=0;
+//
+//    int meth(int num){
+//       while (num > 0){
+//           digit = num % 10;
+//           sum = sum + digit;
+//           num = num /10;
+//       }
+//       return sum;
+//    }
 
-    int meth(int num){
-       while (num > 0){
-           digit = num % 10;
-           sum = sum + digit;
-           num = num /10;
-       }
-       return sum;
+    public void main(int i){
+
     }
+
     public static void main(String[] args) {
         DigitsSum ds = new DigitsSum();
-        System.out.println(ds.meth(123));
+//        System.out.println(ds.meth(123));
 
     }
 }
