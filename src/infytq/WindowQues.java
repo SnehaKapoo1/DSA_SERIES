27afd7bd0e2e6f18dd1d9e548b052254b3a5afd7
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-01-31 18:58:21 +0530

    peak of the min of every contiguous sublist

---
+++ src/infytq/WindowQues.java
@@ -0,0 +1,36 @@
+package infytq;
+
+import java.util.Scanner;
+
+public class WindowQues {
+
+    static void windowMin(int a[], int s)
+    {
+        for (int k = 1; k <= s; k++) {
+            int maxmin = Integer.MIN_VALUE;
+
+            for (int i = 0; i <= s - k; i++) {
+                int min = a[i];
+                for (int j = 1; j < k; j++) {
+                    if (a[i + j] < min)
+                        min = a[i + j];
+                }
+
+                if (min > maxmin)
+                    maxmin = min;
+            }
+            System.out.print(maxmin + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+       // int n = sc.nextInt();
+        //int num[] = new int[7];
+        int num [] = { 10, 20, 30};
+        /*for(int i=0; i<7; i++){
+            num[i] = sc.nextInt();
+        }*/
+        windowMin(num, 3);
+    }
+}
