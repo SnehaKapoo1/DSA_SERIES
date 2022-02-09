3be8d00efceb100ba975b381da758e71a954f323
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-02-09 23:41:45 +0530

    Ransome note

---
+++ src/hacker_rank/array_ques/hashing/RansomNote.java
@@ -0,0 +1,12 @@
+package hacker_rank.array_ques.hashing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RansomNote {
+
+    public static void main(String[] args) {
+        List<String> magzine = new ArrayList<>();
+        List<String> note = new ArrayList<>();
+    }
+}
