cdb9f220e6d0365335e5cf31dedff6f87d170fc6
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-02-10 00:32:20 +0530

    Two String

---
+++ src/hacker_rank/array_ques/hashing/TwoString.java
@@ -0,0 +1,27 @@
+package hacker_rank.array_ques.hashing;
+
+import java.util.HashSet;
+
+public class TwoString {
+    public static String twoStrings(String s1, String s2) {
+        HashSet<Character> hs = new HashSet<>();
+        for(Character c : s1.toCharArray()){
+            hs.add(c);
+        }
+
+        for(Character ele : s2.toCharArray()){
+            if(hs.contains(ele)){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "world";
+
+        System.out.println(twoStrings(s1, s2));
+
+    }
+
+}
