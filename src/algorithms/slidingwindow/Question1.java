896777556eebe82e6bd68762f9cb58c6a2985752
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-02-14 11:56:44 +0530

    sliding window one more way updated

--- src/algorithms/slidingwindow/Question1.java
+++ src/algorithms/slidingwindow/Question1.java
@@ -2,7 +2,7 @@ package algorithms.slidingwindow;
 
 public class Question1 {
 
-    static int find(int arr[], int k){
+    static int bruteForceApproach(int arr[], int k){
 
         int res =0;
         for(int i=0; i< arr.length -k + 1; i++){
@@ -16,9 +16,51 @@ public class Question1 {
         return res;
     }
 
+    static int slidingWindowApp(int arr[], int k){
+
+        int start =0;
+        int max_sum =0;
+        for(start = 0; start<k; start++){
+            max_sum +=arr[start];
+        }
+
+       int window_sum =max_sum;
+        for( int end = k; end < arr.length; end++){
+            window_sum += arr[end] - arr[end - k];
+            max_sum = Math.max(max_sum, window_sum);
+
+        }
+        return max_sum;
+    }
+
+    static int slidingWindowAdityaVermaApproach(int arr[], int window_size){
+        int start =0;
+        int end =0;
+        int sum =0;
+        int max = Integer.MIN_VALUE;
+        int size = arr.length;
+
+        while(end < size){
+            sum +=arr[end];
+
+            if((end - start + 1) < window_size)
+                end++;
+
+            else if((end - start + 1) == window_size){
+                max = Math.max(sum, max);
+                sum = sum - arr[start];
+                start++;
+                end++;
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        int arr[] = {2, 3, 5, 2, 9, 7, 1};
+        int arr[] = {2, 5, 1, 8, 2, 9, 1};
         int k =3;
-        System.out.println(find(arr, k));
+        System.out.println(bruteForceApproach(arr, k));
+        System.out.println(slidingWindowApp(arr, k));
+        System.out.println(slidingWindowAdityaVermaApproach(arr, k));
     }
 }
