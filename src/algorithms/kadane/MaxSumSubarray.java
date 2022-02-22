d3b90d7a203cc9cfa37f74c67c6dbab20ee9d5e1
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-02-22 16:45:41 +0530

    Greedy algorithm

--- src/algorithms/kadane/MaxSumSubarray.java
+++ src/algorithms/kadane/MaxSumSubarray.java
@@ -2,7 +2,7 @@ package algorithms.kadane;
 
 public class MaxSumSubarray {
 
-    static int findMax(int arr[], int n){
+    static int findMaxNaive(int arr[], int n){
         int max =arr[0];
 
         for(int i=0; i<n; i++){
@@ -14,9 +14,23 @@ public class MaxSumSubarray {
         }
         return max;
     }
+
+    static int kadaneAlgo(int[] arr, int n){
+        int res = arr[0];
+        int maxEnding = arr[0];
+
+        for(int i=1; i<n; i++){
+            maxEnding = Math.max(maxEnding+arr[i], arr[i]);
+            res = Math.max(maxEnding, res);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, -2, 3, -1, 2};
         int n = arr.length;
-        System.out.println(findMax(arr, n));
+        System.out.println(findMaxNaive(arr, n));
+        System.out.println(kadaneAlgo(arr, n));
     }
 }
