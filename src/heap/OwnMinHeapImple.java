5e653222b877af5203ee569b522c5233b0a36232
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-03-02 23:17:29 +0530

    Heap

--- src/heap/OwnMinHeapImple.java
+++ src/heap/OwnMinHeapImple.java
@@ -1,5 +1,7 @@
 package heap;
 
+import java.util.Arrays;
+
 public class OwnMinHeapImple {
     int arr[];
     int size;
@@ -23,8 +25,31 @@ public class OwnMinHeapImple {
         return (i-1)/2;
     }
 
+    void insertItems(int ele){
+        if(size == capacity) return;
+        size++;
+        arr[size -1] = ele;
+        for(int i = size-1; i!=0 && arr[parent(i)]>arr[i];){
+            int temp = arr[i];
+            arr[i] = arr[parent(i)];
+            arr[parent(i)] = temp;
+
+            i = parent(i);
+        }
+    }
+
     public static void main(String[] args) {
         OwnMinHeapImple minHeapImple = new OwnMinHeapImple(11);
         System.out.println(minHeapImple.capacity);
+        minHeapImple.insertItems(10);
+        minHeapImple.insertItems(20);
+        minHeapImple.insertItems(15);
+        minHeapImple.insertItems(40);
+        minHeapImple.insertItems(50);
+        minHeapImple.insertItems(100);
+        minHeapImple.insertItems(25);
+       // minHeapImple.insertItems(10);
+        System.out.println(Arrays.toString(minHeapImple.arr));
+
     }
 }
