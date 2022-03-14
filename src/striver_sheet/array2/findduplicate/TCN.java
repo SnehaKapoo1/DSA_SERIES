0cdf700e2b8a1b23d1375ed384b5c4a33064de6c
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-03-14 14:24:33 +0530

    Duplicate element

---
+++ src/striver_sheet/array2/findduplicate/TCN.java
@@ -0,0 +1,23 @@
+package striver_sheet.array2.findduplicate;
+
+public class TCN {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+    public static void main(String args[]) {
+        int arr[] = {1,3,4,2,3};
+        System.out.println("The duplicate element is " + findDuplicate(arr));
+    }
+}
