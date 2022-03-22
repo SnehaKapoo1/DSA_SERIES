e1bba7b4827349f277e9819e7a47292972127ff8
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-03-22 09:16:22 +0530

    Majority | count > n/2

---
+++ src/striver_sheet/array3/majorityn3/Majority_NDiv2.java
@@ -0,0 +1,35 @@
+package striver_sheet.array3.majorityn3;
+
+public class Majority_NDiv2 {
+    public static int majorityElement(int[] nums) {
+        int res =0;
+        int count = 1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[res] == nums[i])
+                count++;
+            else
+                count--;
+
+            if(count == 0){
+                res = i;
+                count = 1;
+            }
+        }
+
+        count =0;
+        for(int j = 0; j<nums.length; j++){
+            if(nums[res] == nums[j])
+                count++;
+        }
+        if(count > nums.length/2)
+            return nums[res];
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr= {1, 2, 3, 4, 3,3,3};
+        int[] arr1 = {1};
+        System.out.println(majorityElement(arr));
+        System.out.println(majorityElement(arr1));
+    }
+}
