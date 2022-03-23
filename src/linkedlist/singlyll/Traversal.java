8f3d12e441b0a92280fe3adc54d562a6fb28a697
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-03-23 18:30:01 +0530

    Linked List

--- src/linkedlist/singlyll/Traversal.java
+++ src/linkedlist/singlyll/Traversal.java
@@ -1,6 +1,5 @@
 package linkedlist.singlyll;
 
-
 class Node{
     int data;
     Node next;
@@ -26,6 +25,31 @@ public class Traversal {
         recursiveDisplay(head.next);
     }
 
+    public static int searchItemIterative(Node head, int item){
+        int pos = 1;
+        Node curr = head;
+        while(curr != null){
+            if(curr.data == item){
+                return pos-1;
+            }else {
+                pos++;
+                curr = curr.next;
+            }
+        }
+        return -1;
+    }
+
+    public static int recursiveSearch(Node head, int item){
+      if(head == null)
+          return -1;
+      if(head.data == item) return 1;
+      else {
+          int result = recursiveSearch(head.next, item);
+          if (result == -1) return -1;
+          else return (result + 1);
+      }
+    }
+
     public static void main(String[] args) {
         Node head = new Node(10);
         head.next = new Node(20);
@@ -34,5 +58,9 @@ public class Traversal {
         printList(head);
         System.out.println();
         recursiveDisplay(head);
+        System.out.println();
+        System.out.println("Searched element is at index" + ": " + searchItemIterative(head, 20));
+        System.out.println();
+        System.out.println("Searched element is at position" + ": " + recursiveSearch(head, 40));
     }
 }
