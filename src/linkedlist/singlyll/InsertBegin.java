cc59b296719534c62c751667544d87c1f15bde8c
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-03-24 11:11:13 +0530

    Insert at end

--- src/linkedlist/singlyll/InsertBegin.java
+++ src/linkedlist/singlyll/InsertBegin.java
@@ -9,6 +9,17 @@ public class InsertBegin {
         return head;
     }
 
+    public static Node insertEnd(Node head, int item){
+        Node temp = new Node(item);
+        if(head == null) return temp;
+        Node curr = head;
+        while(curr.next !=null){
+            curr = curr.next;
+        }
+        curr.next = temp;
+        return head;
+    }
+
     public static void printList(Node head){
         Node curr = head;
         while(curr != null){
@@ -22,6 +33,11 @@ public class InsertBegin {
         head = insertBegin(head, 30);
         head = insertBegin(head, 20);
         head = insertBegin(head, 10);
+        printList(head);
+        System.out.println();
+        head = insertEnd(head, 40);
+        head = insertEnd(head, 50);
+        head = insertEnd(head, 60);
         printList(head);
     }
 }
