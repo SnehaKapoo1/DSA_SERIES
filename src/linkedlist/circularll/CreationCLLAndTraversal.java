5983060419649ac0fdc4522c81a7af393089a9f0
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-03-25 12:48:56 +0530

    Circular Linked List

---
+++ src/linkedlist/circularll/CreationCLLAndTraversal.java
@@ -0,0 +1,30 @@
+package linkedlist.circularll;
+
+class Node{
+    int data;
+    Node next;
+    Node(int ele){
+        data = ele;
+        next = null;
+    }
+}
+public class CreationCLLAndTraversal {
+    public static void printList(Node head){
+        if(head == null || head.next == head) return;
+        System.out.print(head.data + " ");
+      Node curr = head.next;
+        while(curr != head){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = head;
+        printList(head);
+    }
+}
