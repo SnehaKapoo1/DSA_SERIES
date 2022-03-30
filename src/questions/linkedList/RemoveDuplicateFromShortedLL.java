b8c0dba963ec820d791c343cf8fc6a563550819a
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-03-30 11:42:57 +0530

    Remove Duplicate from SLL

---
+++ src/questions/linkedList/RemoveDuplicateFromShortedLL.java
@@ -0,0 +1,16 @@
+package questions.linkedList;
+
+public class RemoveDuplicateFromShortedLL {
+    public static Node removeDupli(Node head){
+        Node curr = head;
+        if(curr == null)
+            return null;
+        while(curr.next != null){
+            if(curr.data == curr.next.data)
+                curr.next = curr.next.next;
+            else
+                curr = curr.next;
+        }
+        return head;
+    }
+}
