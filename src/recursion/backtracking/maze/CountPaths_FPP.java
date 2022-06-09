2b5e1758c78351a6c76fb2e3380ffac343910fd9
SnehaKapoo1 <86736110+SnehaKapoo1@users.noreply.github.com>
2022-06-09 11:25:32 +0530

    approach 2

--- src/recursion/backtracking/maze/CountPaths_FPP.java
+++ src/recursion/backtracking/maze/CountPaths_FPP.java
@@ -8,7 +8,20 @@ public class CountPaths_FPP {
         return countPaths(i, j+1, m, n) + countPaths(i+1, j, m, n);
     }
 
+    static int count =0;
+    static void countPaths2(int i, int j, int m, int n){
+        if(i == m-1 || j == n-1){
+            count++;
+        }
+       else {
+            countPaths2(i, j+1, m, n);
+            countPaths2(i+1, j, m, n);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(countPaths(0, 0, 3 , 3));
+        countPaths2(0,0, 3, 3);
+        System.out.println(count);
     }
 }
